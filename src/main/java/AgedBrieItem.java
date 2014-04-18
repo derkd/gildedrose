aa9825afda13fe6185ea0cf08b8ab1cb443e0283
Derk Dukker <derk.dukker@gmail.com>
2014-04-18 16:44:54 +0200

    Created an AgedBrieItem

---
+++ src/main/java/AgedBrieItem.java
@@ -0,0 +1,5 @@
+public class AgedBrieItem extends Item {
+    public AgedBrieItem() {
+        super("Aged Brie", 2, 0);
+    }
+}
--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -20,7 +20,7 @@ public class GildedRose {
     private void initializeItems() {
         items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new AgedBrieItem());
         items.add(new Item("Elixir of the Mongoose", 5, 7));
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
