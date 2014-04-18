2d92fae391c85a8a542df4f5029433aed5889811
Derk Dukker <derk.dukker@gmail.com>
2014-04-18 09:49:41 +0200

    formatted the code and created initial test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,3 +1,5 @@
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -5,8 +7,10 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
-	}
+    @Test
+    public void shouldCreateInstance() throws Exception {
+        GildedRose gd = new GildedRose();
+
+        assertThat(gd, is(instanceOf(GildedRose.class)));
+    }
 }
